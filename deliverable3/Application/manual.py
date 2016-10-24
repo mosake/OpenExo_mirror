79b918e2bfe9bfe56af86f7f032f323c6408801f
c3liujia <jiah.liu@mail.utoronto.ca>
2016-10-23 22:35:43 -0400

    Functional main
    Main should be functional with available commands. Need to include what
    we have for push.py and re-test

---
+++ deliverable3/Application/manual.py
@@ -0,0 +1,10 @@
+def main():
+    print("This page contains the list of commands that you can execute:\n\n",
+    "1) updated: Lists the planetary host names which has been updated since last commit date.\n\n",
+    "2) push : Commits the updated changes to the respository.\n\n",
+    "3) exit: Terminate the program.\n\n"
+    "Other commands coming soon"
+    
+    
+    
+    )

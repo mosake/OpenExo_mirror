53cfeb9f65f93d7984f7e60fc4c7e0f60ba583f6
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-09 01:12:19 -0500

    Edited gitPush and push XML files

--- Push/csvFilePush.py
+++ Push/csvFilePush.py
@@ -1,17 +1,10 @@
 import ctypes  # An included library with Python install.
 import subprocess
 
-'''
-# As Hanno (an admin), I want a notification on the desktop to appear when there is an update from another catalogue found
-catalogue_name = "Test Catalogue"
-notification_description = catalogue_name + " has been updated."
-ctypes.windll.user32.MessageBoxW(0, notification_description, "Update", 1)
-'''
-def push(csv_file_name="Test csv file"):
+def push(csv_file_name):
     print(subprocess.Popen("git add " +  csv_file_name,
                            shell=True, stdout=subprocess.PIPE).stdout.read())
     print(subprocess.Popen("git commit -m \"" + csv_file_name + " has been added.\"",
                            shell=True, stdout=subprocess.PIPE).stdout.read())  
     print(subprocess.Popen("git push -f origin master",
                            shell=True, stdout=subprocess.PIPE).stdout.read()) 
-    

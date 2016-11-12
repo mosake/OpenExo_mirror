f74940806c74f9b17e63aed4189a72f02de74d2a
mosake <kellysammo@gmail.com>
2016-11-12 03:55:38 -0500

    updated script to compare local repo instead of online catalog

--- deliverable3/databasecmp/databasecmp.py
+++ deliverable3/databasecmp/databasecmp.py
@@ -1,48 +1,63 @@
-'''
-Script from Hanno Rein's github
-Formatted to compare names and alternate names in the exoplanet catalog and 
-a given database of new files
+import glob, os, xml.etree.ElementTree as ET
 
-notes:
-    urllib changed to urllib.request due to update with Python3
-        urllib   ->  urllib.request, urllib.parse, and urllib.error
+'''
+Returns a dictionary with the following format, given a path to a directory:
+{file: [system, star, planet, binary planet]}
+If a field is missing, print None
+
+path - the path of the dictionary containing 1 or more xml files
+'''
+
+def get_dictionary(path):
+    
+    # compare with new files   
+    files=glob.glob(path + "/*.xml")
+
+    cdb = {}
+
+    for file in files:
+        f = open(file, 'r')
+        filename = os.path.basename(file)
+        temp = []
+        current_data = ET.parse(f)
+        systems = current_data.getroot()
+
+        if not current_data.findall(".//planet"):
+            temp.append('None')
+        else:         
+            temp2 = []
+            for system in systems:
+                temp2.append(systems.findtext("name")) 
+            temp.append(temp2)
         
-url: open exoplanet catalog URL
-oec: oec parsed xml trees
-'''
+        if not current_data.findall(".//star"):
+            temp.append('None')
+        else:                
+            temp2 = []
+            for star in current_data.findall(".//star"):
+                temp2.append(star.findtext("name"))   
+            temp.append(temp2)
+            
+        if not current_data.findall(".//planet"):
+            temp.append('None')
+        else:
+            temp2 = []
+            for planet in current_data.findall(".//planet"):
+                temp2.append(planet.findtext("name"))
+            temp.append(temp2)
+            
+        if not current_data.findall(".//binary/planet"):
+            temp.append('None')
+        else:
+            temp2 = []
+            for cbin in current_data.findall(".//binary/planet"):
+                temp2.append(cbin.findtext("name"))     
+            temp.append(temp2)
+        cdb[filename] = temp
+        
+    return cdb
+# end of get_dictionary
 
-import xml.etree.ElementTree as ET, xml.etree.ElementTree as NET, urllib.request, gzip, io, glob
-url = "https://github.com/OpenExoplanetCatalogue/oec_gzip/raw/master/systems.xml.gz"
-oec = ET.parse(gzip.GzipFile(fileobj=io.BytesIO(urllib.request.urlopen(url).read())))
-
-oec_planet = []
-oec_cbin = []
-oec_star = []
-oec_system = []
-
-
-# catalog planet names 
-for planet in oec.findall(".//planet"):
-    oec_planet.append(planet.findtext("name"))
-    
-# catalog circumbinary planet names 
-for cbin in oec.findall(".//binary/planet"):
-    oec_cbin.append(cbin.findtext("name"))
-
-# catalog star names 
-for star in oec.findall(".//star"):
-    oec_star.append(star.findtext("name"))
-    
-# catalog system names 
-for system in oec.findall(".//system"):
-    oec_system.append(system.findtext("name"))
-    
-# determine if name exists    
-    
-# compare with new files
-path = 'C:/Users/Kelly/Desktop/f16/CSCC01/team25-Project/deliverable3/databasecmp/test_database1'   
-files=glob.glob(path)   
-for file in files: 
-    tree = ET.parse(file)
-    
-    oec_planet = []
+current_database = "C:/Users/Kelly/Desktop/f16/CSCC01/team25-Project/deliverable3/databasecmp/test_database1"
+new_database = "C:/Users/Kelly/Desktop/f16/CSCC01/team25-Project/deliverable3/databasecmp/test_database3"
+print(get_dictionary(current_database))

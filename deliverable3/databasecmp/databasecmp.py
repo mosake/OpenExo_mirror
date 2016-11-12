93455856d0527277071687c252d4385ce645650e
mosake <kellysammo@gmail.com>
2016-11-12 04:41:53 -0500

    completed compare_function with light testing

--- deliverable3/databasecmp/databasecmp.py
+++ deliverable3/databasecmp/databasecmp.py
@@ -2,7 +2,7 @@ import glob, os, xml.etree.ElementTree as ET
 
 '''
 Returns a dictionary with the following format, given a path to a directory:
-{file: [system, star, planet, binary planet]}
+{file: [[system], [star], [planet], [binary planet]]}
 If a field is missing, print None
 
 path - the path of the dictionary containing 1 or more xml files
@@ -58,6 +58,54 @@ def get_dictionary(path):
     return cdb
 # end of get_dictionary
 
+'''
+Given 2 dictionaries of the form:
+{file: [[system], [star], [planet], [binary planet]]}
+(If a field is missing, print None)
+
+Check if each file has distinct values between the chosen value.
+
+Returns a list of filenames that are already in both files.
+If find is invalid do nothing.
+
+dict1, dict2 - dictionaries
+find - system, star, planet or bplanet
+       default: "planet"
+'''
+
+def check_repetitions(dict1, dict2, find="planet"):
+    current_index = None
+    repeating_files = []    
+    
+    if find == "system":
+        current_index = 0
+    elif find == "star":
+        current_index = 1
+    elif find == "planet":
+        current_index = 2
+    elif find == "bplanet":
+        current_index = 3
+    else:
+        return None
+    
+    for key in dict1:
+        for i in key[current_index]:
+            for key2 in dict2:
+                # check if filenames repeat
+                if key == key2:
+                    repeating_files.append(key)
+                else:
+                    for j in key[current_index]:
+                        if i == j:
+                            repeating_files.append(key)
+                            break
+    return repeating_files
+#end of check_repetitions
+
+
 current_database = "C:/Users/Kelly/Desktop/f16/CSCC01/team25-Project/deliverable3/databasecmp/test_database1"
 new_database = "C:/Users/Kelly/Desktop/f16/CSCC01/team25-Project/deliverable3/databasecmp/test_database3"
-print(get_dictionary(current_database))
+cdb = get_dictionary(current_database)
+ndb = get_dictionary(new_database)
+
+print(check_repetitions(cdb, ndb, "planet"))

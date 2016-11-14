fc4459dc3bf4f00a0923d79daab030770149b955
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-14 02:56:13 -0500

    Push to main repository

--- Application/main.py
+++ Application/main.py
@@ -1,28 +1,7 @@
-<<<<<<< HEAD
-switch = True
-
-while (switch):
-
-    #Prompt user to input (i.e. help)
-    command = input('>>> ')
-
-    #Redirects the main to call other python scripts accordingly
-    #Refer to help page for details of each command
-    if (command == "help"):
-        import manual
-    elif (command[0:2] == "up"):
-        import update
-        update.main(command)
-    elif (command == "exit"):
-        switch = False
-
-
-=======
 import list_updated as list_updated
 import manual as man
 import translate_ExoPlanet as translate_Exoplanet
 import translate_NASA as translate_NASA
-
 
 switch = True
 while (switch):
@@ -42,10 +21,8 @@ while (switch):
         translate_NASA.get()
         translate_NASA.parse()
     elif (command[0:4] == "push"):
-        import gitPush as gitPush
-        gitPush.push_all()
+        push.main()
     elif (command == "exit"):
         switch = False
     else:
         print("'"+command + "' is not an available command. Enter 'help' for list of available commands.")
->>>>>>> 4ef1fa568bf5eb81f087dde41b146f397d7a403a

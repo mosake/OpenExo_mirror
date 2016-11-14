a295173efbac50ec15ff0c9cff0849c7e7f6d472
kriskirla <kris.lai@mail.utoronto.ca>
2016-11-14 03:35:35 -0500

    Updated Main

--- deliverable4/Application/manual.py
+++ deliverable4/Application/manual.py
@@ -1,8 +1,9 @@
 def main():
     print("This page contains the list of commands that you can execute:\n\n",
     "1) updated: Lists the planetary host names which has been updated since last commit date.\n\n",
-    "2) push : Commits the updated changes to the respository.\n\n",
-    "3) exit: Terminate the program.\n\n"
+    "2) compare : Compares two xmxl files and output to a csv file.\n\n",
+    "3) push : Commits the updated changes to the respository.\n\n",
+    "4) exit: Terminate the program.\n\n"
     "Other commands coming soon"
 
 

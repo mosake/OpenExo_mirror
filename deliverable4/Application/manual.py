7787d668620cdc6f606e8f9e02775cd77a21ea56
kriskirla <kris.lai@mail.utoronto.ca>
2016-11-14 02:43:16 -0500

    Updated Manual

--- deliverable4/Application/manual.py
+++ deliverable4/Application/manual.py
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-print("This page contains the list of commands that you can execute:\n\n",
-"1) up <url>: Updates the respository with the given url respository.\n\n",
-"2) cm -m \"message\": Commits the updated changes to the respository.\n\n",
-"3) exit: Terminate the program.\n\n"
-"In Progress"
-
-
-
-)
-=======
 def main():
     print("This page contains the list of commands that you can execute:\n\n",
     "1) updated: Lists the planetary host names which has been updated since last commit date.\n\n",
     "2) push : Commits the updated changes to the respository.\n\n",
     "3) exit: Terminate the program.\n\n"
     "Other commands coming soon"
-    
-    
-    
+
+
+
     )
->>>>>>> 4ef1fa568bf5eb81f087dde41b146f397d7a403a

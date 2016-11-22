c05dfc61dbdb91fa311f1b851d3023d319612935
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-22 14:41:31 -0500

    Push to main repository

--- deliverable4/Application/gitPush.py
+++ deliverable4/Application/gitPush.py
@@ -18,16 +18,17 @@ def push_all():
             pull_result = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             pull_output = pull_result.communicate()[0]
             pull_errors = pull_result.communicate()[1]
-            potential_error_msg = pull_errors.decode()
-            if(potential_error_msg != ""):
-                if("files would be overwritten by merge" in potential_error_msg):
+            error_message = pull_errors.decode()
+            if(error_message != ""):
+                if("files would be overwritten by merge" in error_message):
                     # There is a merge conflict in pulling the master repo
                     # Any merge conflict would need to be solved manually
                     # (That way, no data is accidentally lost)
                     print("Oops! There seems to be a merge conflict.")
                     print("Please check your files and the master repository.")
                     raise
-                elif("Aborting" in potential_error_message):
+                elif("Aborting" in error_message):
+                    # Some other error has occurred.
                     print("The master repository could not be pulled.")
                     raise
          
@@ -57,8 +58,8 @@ def push_all():
             subprocess.Popen("git commit -m \"Push to main repository\"",
                                    shell=True,
                                    stdout=subprocess.PIPE).stdout.read()
-            print(subprocess.Popen("git push origin master", shell=True,
-                                   stdout=subprocess.PIPE).stdout.read())
+            subprocess.Popen("git push origin master", shell=True,
+                                   stdout=subprocess.PIPE).stdout.read()
             print('\n')
     except:
         print("Git was unable to push your local copy to the main repository.")

bf2df2ade793d6442a2d4cd2938ad5b57a120deb
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-22 10:35:36 -0500

    Push to main repository

--- deliverable4/Application/gitPush.py
+++ deliverable4/Application/gitPush.py
@@ -22,7 +22,7 @@ def push_all():
             if(bytes("Aborting", encoding="ascii") in subprocess.Popen("git push origin master", shell=True,
                                    stdout=subprocess.PIPE).stdout.read()):
                 print("error")
-            print('Repository has been successfully pushed.\n')
+            print('\n')
         elif platform.system() == "Linux":
             # Command if run on Linux device (Could be subject to change)
             subprocess.Popen("git pull origin master", shell=True,
@@ -34,7 +34,7 @@ def push_all():
                                    stdout=subprocess.PIPE).stdout.read()
             print(subprocess.Popen("git push origin master", shell=True,
                                    stdout=subprocess.PIPE).stdout.read())
-            print('Repository has been successfully pushed.\n')
+            print('\n')
         elif platform.system() == "Darwin" or platform.system() == "darwin":
             subprocess.Popen("git pull origin master", shell=True,
                                    stdout=subprocess.PIPE).stdout.read()
@@ -45,6 +45,6 @@ def push_all():
                                    stdout=subprocess.PIPE).stdout.read()
             print(subprocess.Popen("git push origin master", shell=True,
                                    stdout=subprocess.PIPE).stdout.read())
-            print('Repository has been successfully pushed.\n')
+            print('\n')
     except:
         print("Git was unable to push your local copy to the main repository.")

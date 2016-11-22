3c9e296733699ea60a8d0905048124a9642325fe
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-22 12:51:06 -0500

    test

--- deliverable4/Application/gitPush.py
+++ deliverable4/Application/gitPush.py
@@ -12,21 +12,20 @@ def push_all():
         if platform.system() == "Windows":
 
             subprocess.Popen("git checkout master", shell=True, stdout=subprocess.PIPE)
-    #        subprocess.Popen("git pull origin master", shell=True,
-    #                               stdout=subprocess.PIPE).stdout.read()
+
             
             pull_result = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
             pull_output = pull_result.communicate()[0]
-
+            print("*********")
             print(pull_output.decode())
-            
+            print("*********")
+            #        subprocess.Popen("git pull origin master", shell=True,
+            #                               stdout=subprocess.PIPE).stdout.read()            
             subprocess.Popen("git add *", shell=True, stdout=subprocess.PIPE)
             subprocess.Popen("git commit -m \"Push to main repository\"",
                                    shell=True, stdout=subprocess.PIPE)
-   #         subprocess.Popen("git push origin master", shell=True,
-    #                               stdout=subprocess.PIPE).stdout.read()
-            push_result = subprocess.Popen(["git", "push"], stdout=subprocess.PIPE)
-            push_output = push_result.communicate()[0]
+            subprocess.Popen("git push origin master", shell=True,
+                                   stdout=subprocess.PIPE).stdout.read()
             print('\n')
         elif platform.system() == "Linux":
             # Command if run on Linux device (Could be subject to change)

810078ce405c02687aa900f564e21d052b1022cb
Angelina Choi <angelina.choi@mail.utoronto.ca>
2016-11-30 19:10:43 -0500

    Push to main repository

--- deliverable5/Application/gitPush.py
+++ deliverable5/Application/gitPush.py
@@ -5,9 +5,7 @@ import time
 '''
 push_all() pushes changes from local repository to master using command line
 '''
-
-
-def push_all(repo_path=os.getcwd(), directory="*"):
+def pull_repo(repo_path=os.getcwd()):
     platform_names = ['Windows', 'Linux', 'Darwin', 'darwin']
     try:
         if platform.system() in platform_names:
@@ -39,36 +37,43 @@ def push_all(repo_path=os.getcwd(), directory="*"):
                     # Any merge conflict would need to be solved manually
                     # (That way, no data is accidentally lost)
                     print("Oops! There seems to be a merge conflict.")
-                    print("Please check your files and the master repository.")
+                    print("Please check your files and the repository.")
                     raise
                 elif("Aborting" in pull_error_msg):
                     # Some other error has occurred.
-                    print("The master repository could not be pulled.")
+                    print("The repository could not be pulled.")
                     raise
-            add_command = subprocess.Popen("git add \"" + directory + "\"",
+            print("Repository has been successfully pull")
+    except:
+        print("Git was unable to pull the repository.")
+
+
+def push_all(repo_path=os.getcwd(), directory="*"):
+    platform_names = ['Windows', 'Linux', 'Darwin', 'darwin']
+    try:
+        add_command = subprocess.Popen("git -C " + repo_path + " add \"" + directory + "\"",
                                            stdout=subprocess.PIPE,
                                                 stderr=subprocess.PIPE)
-            add_output, add_err = add_command.communicate()
-            add_error_msg = add_err.decode()
-            print(add_error_msg)
-            if ("fatal" in add_error_msg):
-                print("Oops! Are you adding an invalid file/directory?")
-                raise
-            commit_command = subprocess.Popen("git commit"\
+        add_output, add_err = add_command.communicate()
+        add_error_msg = add_err.decode()
+        if ("fatal" in add_error_msg):
+            print("Oops! Are you adding an invalid file/directory?")
+            raise
+        commit_command = subprocess.Popen("git -C " + repo_path + " commit"\
                                               " -m \"Push to main repository\"",
                                               shell=True,
                                               stdout=subprocess.PIPE)
-            commit_command.communicate()
-            push_command = subprocess.Popen("git push origin " + branch_name,
+        commit_command.communicate()
+        push_command = subprocess.Popen("git -C " + repo_path + " push origin " + branch_name,
                                             shell=True,
                                             stdout=subprocess.PIPE,
                                             stderr=subprocess.PIPE)
-            push_output, push_error = push_command.communicate()
-            push_error_message = push_error.decode()
-            if(push_error_message != "" and "error" in push_error_message):
-                print("Oops! A conflict occurred when pushing to remote.")
-                print("Please check the repositories for further details.")
-                raise
-            print("Push was successful.")
+        push_output, push_error = push_command.communicate()
+        push_error_message = push_error.decode()
+        if(push_error_message != "" and "error" in push_error_message):
+            print("Oops! A conflict occurred when pushing to remote.")
+            print("Please check the repositories for further details.")
+            raise
+        print("Push was successful.")
     except:
         print("Git was unable to push your local copy to the main repository.")
